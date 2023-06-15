a2917bd2416c7fb41dc7da74e49e4565bb811505
chuchilini <vanesalees@gmail.com>
2023-06-15 13:24:07 +0300

    --all

--- parsing.c
+++ parsing.c
@@ -5,9 +5,10 @@
 1. Readline using readline function
 2. lexical analysis
 	2-1)skip whitespace
-	2-2)deal with quotation marks
+	2-2)deal with quotation marks(?)
 	2-3)identify token
-Check grammar rules: https://pubs.opengroup.org/onlinepubs/009604499/utilities/xcu_chap02.html#tag_02_10_02
+3. syntax analysis
+	3-1)Check grammar rules: https://pubs.opengroup.org/onlinepubs/009604499/utilities/xcu_chap02.html#tag_02_10_02
 */
 
 
@@ -27,43 +28,75 @@ typedef struct s_token{
 	char	*str;
 }	t_token;
 
-/*if input: <file cat | grep ""
+/*if input: <file cat | grep " "
 
-str: "<", "file", "cat", "|", "grep", """", NULL
+str: "<", "file", "cat", "|", "grep", "" "", NULL
 type: REDIRECT, WORD, WORD, PIPE, WORD, WORD, NULL*/
 
-void	handler(int signum)
-{
-	if (signum != SIGINT)
-		return ;
-	printf("ctrl + c\n");
-	rl_on_new_line();
-	rl_replace_line("", 1);
-	rl_redisplay();
-}
 
-int	main(void)
-{
-	int		ret;
-	char	*line;
+/*--------------syntax analysis------------------*/
 
-	ret = 0;
-	signal(SIGINT, handler);
-	while (1)
-	{
-		line = readline("input> ");
-		if (line)
-		{
-			if (ret)
-				printf("output> %s\n", line);
-			add_history(line);
-			free(line);
-			line = NULL;
-		}
-		else
-		{
-			printf("ctrl + d\n");
-		}
-	}
-	return (0);
-}
+// <pipeline>    :	 <cmd>
+//               |   <pipeline> '|' <cmd>
+
+// <cmd>          : <simple_cmd> 
+//                |   <simple_cmd> <redirects>
+
+// <simple_cmd>   : <file_path>
+//                |   <argv>
+
+// <argv>         : <file_path> <args>
+
+// <redirects>    : <io_redirect>
+//                 |  <redirects> <io_redirect>
+
+// <io_redirect>  : '<'   <filename>
+//                |  '<<'  <filename>
+//                |  '>'   <filename>
+//                |  '>>'  <filename>
+
+// <args>        : WORD 
+//               | <args> WORD
+
+// <filename>    : WORD
+
+// <file_path>   : WORD
+
+
+
+//-----------------not related------------------
+// void	handler(int signum)
+// {
+// 	if (signum != SIGINT)
+// 		return ;
+// 	printf("ctrl + c\n");
+// 	rl_on_new_line();
+// 	rl_replace_line("", 1);
+// 	rl_redisplay();
+// }
+
+// int	main(void)
+// {
+// 	int		ret;
+// 	char	*line;
+
+// 	ret = 0;
+// 	signal(SIGINT, handler);
+// 	while (1)
+// 	{
+// 		line = readline("input> ");
+// 		if (line)
+// 		{
+// 			if (ret)
+// 				printf("output> %s\n", line);
+// 			add_history(line);
+// 			free(line);
+// 			line = NULL;
+// 		}
+// 		else
+// 		{
+// 			printf("ctrl + d\n");
+// 		}
+// 	}
+// 	return (0);
+// }
