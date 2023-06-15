9e9b0678c495ce7290c964c219ebd24b5ad31ae4
chuchilini <vanesalees@gmail.com>
2023-06-15 11:34:01 +0300

    parsing ideas

---
+++ parsing.c
@@ -0,0 +1,69 @@
+
+#include "minishell.h"
+
+/* parsing order
+1. Readline using readline function
+2. lexical analysis
+	2-1)skip whitespace
+	2-2)deal with quotation marks
+	2-3)identify token
+Check grammar rules: https://pubs.opengroup.org/onlinepubs/009604499/utilities/xcu_chap02.html#tag_02_10_02
+*/
+
+
+
+/*--------------lexical analysis------------------*/
+/*T as token*/
+
+#define T_NULL 0
+#define T_WORD 1
+#define T_REDIRECT 2
+#define T_PIPE 3
+#define T_DOUBLE_QUOTES 4
+#define T_SINGLE_QUOTES 5
+
+typedef struct s_token{
+	int		type;
+	char	*str;
+}	t_token;
+
+/*if input: <file cat | grep ""
+
+str: "<", "file", "cat", "|", "grep", """", NULL
+type: REDIRECT, WORD, WORD, PIPE, WORD, WORD, NULL*/
+
+void	handler(int signum)
+{
+	if (signum != SIGINT)
+		return ;
+	printf("ctrl + c\n");
+	rl_on_new_line();
+	rl_replace_line("", 1);
+	rl_redisplay();
+}
+
+int	main(void)
+{
+	int		ret;
+	char	*line;
+
+	ret = 0;
+	signal(SIGINT, handler);
+	while (1)
+	{
+		line = readline("input> ");
+		if (line)
+		{
+			if (ret)
+				printf("output> %s\n", line);
+			add_history(line);
+			free(line);
+			line = NULL;
+		}
+		else
+		{
+			printf("ctrl + d\n");
+		}
+	}
+	return (0);
+}
