bc7ca227417cf59aef20c2a36b1fa3239a902f7e
Meri Eskelinen <meskelin@c1r5p19.hive.fi>
2023-07-03 16:23:52 +0300

    Fix norminette

--- headers/env.h
+++ headers/env.h
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meskelin <meskelin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/03 15:55:08 by meskelin          #+#    #+#             */
+/*   Updated: 2023/07/03 16:06:30 by meskelin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ENV_H
-    #define ENV_H
+# define ENV_H
 
-#include "hashmap.h"
+# include "hashmap.h"
 
-typedef struct s_env 
+typedef struct s_env
 {
-    struct s_node **vars;
-}   t_env;
+	struct s_node	**vars;
+}	t_env;
 
-void    fill_env(char **envp, t_env **env);
+void	fill_env(char **envp, t_env **env);
 
-#endif  
+#endif
--- libft/libft.h
+++ libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emmameinert <emmameinert@student.42.fr>    +#+  +:+       +#+        */
+/*   By: meskelin <meskelin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 17:07:21 by meskelin          #+#    #+#             */
-/*   Updated: 2023/06/28 19:55:28 by emmameinert      ###   ########.fr       */
+/*   Updated: 2023/07/03 16:01:17 by meskelin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 # include <unistd.h>
 # include <stdio.h>
 
-
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void	*b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
@@ -26,6 +25,6 @@ void	*ft_calloc(size_t nmemb, size_t size);
 char	**ft_split(char const *s, char c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
-int     ft_print_array(char **str);
+int		ft_print_array(char **str);
 
 #endif
