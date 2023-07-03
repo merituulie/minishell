50fa3122258db0986d11809a121ccc85ed2dc688
Meri Eskelinen <meskelin@c1r5p19.hive.fi>
2023-07-03 19:49:00 +0300

    Fix norminette

--- src/command_handler.c
+++ src/command_handler.c
@@ -6,60 +6,44 @@
 /*   By: meskelin <meskelin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 17:39:58 by meskelin          #+#    #+#             */
-/*   Updated: 2023/07/03 19:06:31 by meskelin         ###   ########.fr       */
+/*   Updated: 2023/07/03 19:48:42 by meskelin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
 
-// if isPiped = 1, we dont need to print anything, we just return whatever the output of the current command is
+// if isPiped = 1, we dont need to print anything,
+// we just return whatever the output of the current command is
 // so that the next command can take it as input.
-static char	*handle_command(t_command *command, t_env **env, char *input, int isPiped)
+static char	*handle_command(t_command *command,
+			t_env **env, char *input, int isPiped)
 {
 	if (!input)
-		printf("%s", input); // just printing input here or otherwise itll complain
+		printf("%s", input); // just printing here or otherwise itll complain
 	if (ft_strncmp(command->command, "env", 3) == 0)
 		return (ft_env(env, isPiped));
 	else if (ft_strncmp(command->command, "echo", 4) == 0)
-	{
-		// function call to echo with the -n flag
-		return (NULL); // here we would actually return the echo output string ready to be printed
-	}
-	else if (ft_strncmp(command->command, "cd", 2) == 0)
-	{
-		// well here we have to actually move the location so, interesting to see how this works
-		return (NULL); // we might need to handle this a different way->-> or not!
-	}
-	else if (ft_strncmp(command->command, "pwd", 3) == 0)
-	{
-		// function call to our pwd implementation
-		return (NULL); // here we would actually return the pwd output string ready to be printed
-	}
-	else if (ft_strncmp(command->command, "export", 6) == 0)
-	{
-		// function call to export impl, do the things we need to aaand
-		return (NULL); // here we would actually return the export output (?) string ready to be printed
-	}
-	else if (ft_strncmp(command->command, "unset", 5) == 0)
-	{
-		// function call to unset impl, do the things we need to aaand
-		return (NULL); // here we would actually return the unset output (?) string ready to be printed
-	}
-	else if (ft_strncmp(command->command, "exit", 4) == 0)
-	{
-		// function call to exit impl, do the things we need to
 		return (NULL);
-	}
+	else if (ft_strncmp(command->command, "cd", 2) == 0)
+		return (NULL);
+	else if (ft_strncmp(command->command, "pwd", 3) == 0)
+		return (NULL);
+	else if (ft_strncmp(command->command, "export", 6) == 0)
+		return (NULL);
+	else if (ft_strncmp(command->command, "unset", 5) == 0)
+		return (NULL);
+	else if (ft_strncmp(command->command, "exit", 4) == 0)
+		return (NULL);
 	else
 	{
-		// if the command given is none of our own ones,
-		// we use execp or whatever it is to use the actual command here
-		// and return the output of that?
+		/* if the command given is none of our own ones,
+			we use execp or whatever it is to use the actual command here
+			and return the output of that? */
 	}
 	return (NULL);
 }
 
-int handle_commands(t_command *commands[], t_env **env)
+int	handle_commands(t_command *commands[], t_env **env)
 {
 	int			i;
 	t_command	*next;
@@ -71,19 +55,9 @@ int handle_commands(t_command *commands[], t_env **env)
 	{
 		next = commands[i + 1];
 		if (next)
-		{
-			if (output)
-				output = handle_command(commands[i], env, output, 1);
-			else
-				output = handle_command(commands[i], env, NULL, 1);
-		}
+			output = handle_command(commands[i], env, output, 1);
 		else
-		{
-			if (output)
-				output = handle_command(commands[i], env, output, 0);
-			else
-				output = handle_command(commands[i], env, NULL, 0);
-		}
+			output = handle_command(commands[i], env, output, 0);
 		i++;
 	}
 	return (0);
