f0e6aa44f9b035357051005b0db0cbe3c2900a40
Josefina Husso <husso.josefina@gmail.com>
2023-07-04 15:01:08 +0300

    mmm

--- libft/ft_free_array.c
+++ libft/ft_free_array.c
@@ -6,7 +6,7 @@
 /*   By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 14:33:36 by jhusso            #+#    #+#             */
-/*   Updated: 2023/07/04 10:12:26 by jhusso           ###   ########.fr       */
+/*   Updated: 2023/07/04 14:38:00 by jhusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@ void	ft_free_array(char **array)
 	int	i;
 
 	i = 0;
-	while (array[i])
+	while (array[i] != '\0')
 	{
 		printf("in free array getting freed: array[%i]: %s p: %p\n", i, array[i], &(*array[i]));
 		free (array[i]);
+		array[i] = NULL;
 		i++;
 	}
 	free (array);
--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 14:49:55 by jhusso            #+#    #+#             */
-/*   Updated: 2023/07/04 11:21:45 by jhusso           ###   ########.fr       */
+/*   Updated: 2023/07/04 14:54:49 by jhusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ char **allocate_2d_array(char **old_array)
 	size_t	arr_len;
 
 	arr_len = ft_arrlen(old_array);
-	new_array = (char **)ft_calloc(arr_len + 2, sizeof(char *));
+	new_array = ft_calloc(arr_len + 2, sizeof(char *));
 	if (!new_array)
 		return (NULL);
 	return (new_array);
@@ -71,25 +71,28 @@ char	**add_line(char **old_array, size_t len, size_t del_index, int del_line_ind
 	i = 0;
 	if (del_line_index == 0)
 	{
-		new_array[0] = ft_substr(old_array[0], 0, del_index);
+		new_array[i++] = ft_substr(old_array[i], 0, del_index);
 		printf("HWERE\n");
 		// printf("len of new_array[%i]: %i\n", i, (ft_strlen(old_array[i-1]) - del_index));
-		new_array[1] = ft_substr(old_array[0], del_index, (ft_strlen(old_array[0]) - del_index));
+		new_array[i] = ft_substr(old_array[i - 1], del_index, (ft_strlen(old_array[i - 1]) - del_index));
 		printf("HWERE\n");
 		// printf("old_array[%i]: %s, del_index: %i\n", i - 1, old_array[i-1], del_index);
 
 	}
-	while (i < ft_arrlen(old_array))
-	{
-		if (i == del_line_index)
+	// else
+	// {
+		while (i < ft_arrlen(old_array))
 		{
-			new_array[i] = ft_substr(old_array[i], 0, del_index);
-			new_array[i+1] = ft_substr(old_array[i], del_index + 1, ft_strlen(old_array[i]));
+			if (i == del_line_index)
+			{
+				new_array[i] = ft_substr(old_array[i], 0, del_index);
+				new_array[i+1] = ft_substr(old_array[i], del_index + 1, (ft_strlen(old_array[i]) - del_index));
+			}
+			else
+				new_array[i] = ft_strdup(old_array[i]);
+			i++;
 		}
-		else
-			new_array[i] = ft_strdup(old_array[i]);
-		i++;
-	}
+	// }
 	ft_free_array(old_array);
 	return (new_array);
 }
@@ -114,8 +117,10 @@ char	**parse_line(char **array, size_t len)
 				printf("arrlen: %i\n", arrlen);
 				// printf("array[%i]: %s p: %p\n", i, array[i], &(*array[i]));
 				array = add_line(array, arrlen, j, i);
+				printf("array[i + 1] = %s\n", array[i + 1]);
 				if (is_delim(array[i + 1][0]) == true)
 					trim_last_line(array, i + 1);
+				printf("array[i + 1] after trim = %s\n", array[i + 1]);
 			}
 			j++;
 		}
@@ -141,10 +146,11 @@ char	**ft_lexer(char *str)
 	trimmed_str = ft_strtrim(str, " \t");
 	len = ft_strlen(trimmed_str);
 	printf("trimmed string len: %i\n", len);
-	new_str = (char **)ft_calloc(2, sizeof(char *));
+	new_str = ft_calloc(2, sizeof(*new_str));
 	if (!new_str)
 		return (0);
 	new_str[0] = ft_strdup(trimmed_str);
 	parsed_line = parse_line(new_str, len);
-	ft_free_array(new_str);
+	printf("SEGFAULT\n");
+	// ft_free_array(new_str);
 }
