7d8b731f9c25194e351e4bb3bdecfd89d5f328a1
Rosa-Maria Makinen <rmakinen@c3r5p5.hive.fi>
2023-07-07 10:16:09 +0300

    norminetted

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rmakinen <rmakinen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 17:49:28 by meskelin          #+#    #+#             */
-/*   Updated: 2023/07/06 09:56:52 by rmakinen         ###   ########.fr       */
+/*   Updated: 2023/07/07 10:12:01 by rmakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,5 @@
 
 int	main(void)
 {
-  t_command command;
-
-  command.command_flags = "this is the command okay";
-
-	ft_echo(&command, 0);
-  return (0);
+	return (0);
 }
