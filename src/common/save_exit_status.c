4cf85876f7ee762f6fb8082f8d25d7e5cddc087a
chuchilini <vanesalees@gmail.com>
2023-07-24 11:05:57 +0300

    --all

--- src/common/save_exit_status.c
+++ src/common/save_exit_status.c
@@ -6,7 +6,7 @@
 /*   By: yoonslee <yoonslee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 09:50:22 by yoonslee          #+#    #+#             */
-/*   Updated: 2023/07/24 10:58:29 by yoonslee         ###   ########.fr       */
+/*   Updated: 2023/07/24 11:05:43 by yoonslee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,9 @@ void	we_exit(int number, t_env *env)
 	if (!get_value(&temp, "?"))
 		set_value(&temp, "?", nbr);
 	else
+	{
 		temp = get_value(&temp, "?");
 		temp->value = nbr;
+	}
+	exit(number);
 }
