4b82f46584a23b2db04f4ad931e61630472060a8
Josefina Husso <husso.josefina@gmail.com>
2023-08-01 13:06:29 +0300

    cd edge case fixed, leaks fixed

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,36 +6,12 @@
 /*   By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 09:57:40 by yoonslee          #+#    #+#             */
-/*   Updated: 2023/08/01 11:39:18 by jhusso           ###   ########.fr       */
+/*   Updated: 2023/08/01 13:04:33 by jhusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 #include "../../libft/libft.h"
-
-static void	print_command(t_command *cmd)
-{
-	int	i;
-
-	i = -1;
-	while (cmd[++i].command)
-	{
-		if (cmd[i].command)
-			printf("cmd[%d].command is %s$\n", i, cmd[i].command);
-		if (cmd[i].full_cmd[0])
-			printf("cmd[%d].full_cmd[0] is %s$\n", i, cmd[i].full_cmd[0]);
-		if (cmd[i].full_cmd[1])
-			printf("cmd[%d].full_cmd[1] is %s$\t%p\n", i, cmd[i].full_cmd[1], &(*cmd[i].full_cmd[1]));
-		if (cmd[i].flags)
-			printf("cmd[%d].flags is %s$\n", i, cmd[i].flags);
-		if (cmd[i].input)
-			printf("cmd[%d].input is %s$\t%p\n", i, cmd[i].input, &(*cmd[i].input));
-		if (cmd[i].infile_name)
-			printf("cmd[%d].infile is %s$\n", i, cmd[i].infile_name);
-		if (cmd[i].outfile_name)
-			printf("cmd[%d].outfile is %s$\n", i, cmd[i].outfile_name);
-	}
-}
 
 //first time when cd is called, oldPWD is created, from second onwards, update.
 //error code 1
@@ -61,7 +37,6 @@ static void	go_dir(t_env **env, t_command *command)
 	char	*path;
 
 	temp = *((*env)->vars);
-	// printf("temp = %p\n", &(*temp));
 	if (command->input == NULL)
 	{
 		temp = get_value((*env)->vars, "HOME");
@@ -97,12 +72,9 @@ void	ft_cd(t_command *command, t_env **env)
 		free(temp->value);
 		temp->value = old_pwd;
 	}
-	// free(old_pwd);
-	print_command(command);
 	go_dir(env, command);
 	temp = NULL;
 	temp = get_value((*env)->vars, "PWD");
-	printf("temp value = %s\t%p\n", temp->value, &(*temp->value));
 	free(temp->value);
 	temp->value = getcwd(NULL, 0);
 	if (!temp->value)
