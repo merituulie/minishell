0700b623897a121f07861355daccb8545ad694be
Josefina Husso <husso.josefina@gmail.com>
2023-08-01 11:44:56 +0300

    cd fixes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 17:49:28 by meskelin          #+#    #+#             */
-/*   Updated: 2023/08/01 10:50:51 by jhusso           ###   ########.fr       */
+/*   Updated: 2023/08/01 11:23:48 by jhusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,29 +29,29 @@
 // 	}
 // }
 
-static void	print_command(t_command *cmd)
-{
-	int	i;
+// static void	print_command(t_command *cmd)
+// {
+// 	int	i;
 
-	i = -1;
-	while (cmd[++i].command)
-	{
-		if (cmd[i].command)
-			printf("cmd[%d].command is %s$\n", i, cmd[i].command);
-		if (cmd[i].full_cmd[0])
-			printf("cmd[%d].full_cmd[0] is %s$\n", i, cmd[i].full_cmd[0]);
-		if (cmd[i].full_cmd[1])
-			printf("cmd[%d].full_cmd[1] is %s$\n", i, cmd[i].full_cmd[1]);
-		if (cmd[i].flags)
-			printf("cmd[%d].flags is %s$\n", i, cmd[i].flags);
-		if (cmd[i].input)
-			printf("cmd[%d].input is %s$\n", i, cmd[i].input);
-		if (cmd[i].infile_name)
-			printf("cmd[%d].infile is %s$\n", i, cmd[i].infile_name);
-		if (cmd[i].outfile_name)
-			printf("cmd[%d].outfile is %s$\n", i, cmd[i].outfile_name);
-	}
-}
+// 	i = -1;
+// 	while (cmd[++i].command)
+// 	{
+// 		if (cmd[i].command)
+// 			printf("cmd[%d].command is %s$\n", i, cmd[i].command);
+// 		if (cmd[i].full_cmd[0])
+// 			printf("cmd[%d].full_cmd[0] is %s$\n", i, cmd[i].full_cmd[0]);
+// 		if (cmd[i].full_cmd[1])
+// 			printf("cmd[%d].full_cmd[1] is %s$\n", i, cmd[i].full_cmd[1]);
+// 		if (cmd[i].flags)
+// 			printf("cmd[%d].flags is %s$\n", i, cmd[i].flags);
+// 		if (cmd[i].input)
+// 			printf("cmd[%d].input is %s$\t%p\n", i, cmd[i].input, &(*cmd[i].input));
+// 		if (cmd[i].infile_name)
+// 			printf("cmd[%d].infile is %s$\n", i, cmd[i].infile_name);
+// 		if (cmd[i].outfile_name)
+// 			printf("cmd[%d].outfile is %s$\n", i, cmd[i].outfile_name);
+// 	}
+// }
 
 // static void	print_full_command(t_command *cmd)
 // {
@@ -109,7 +109,7 @@ void	minishell(t_data *ms)
 		if (cmd_line == NULL)
 			continue ;
 		cmd = ft_parser(ms, cmd_line);
-		print_command(cmd);
+		// print_command(cmd);
 		if (cmd == NULL)
 			continue ;
 		execute_commands(cmd, ms->struct_count, &ms->env);
--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,12 +6,36 @@
 /*   By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 09:57:40 by yoonslee          #+#    #+#             */
-/*   Updated: 2023/08/01 10:44:19 by jhusso           ###   ########.fr       */
+/*   Updated: 2023/08/01 11:39:18 by jhusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 #include "../../libft/libft.h"
+
+static void	print_command(t_command *cmd)
+{
+	int	i;
+
+	i = -1;
+	while (cmd[++i].command)
+	{
+		if (cmd[i].command)
+			printf("cmd[%d].command is %s$\n", i, cmd[i].command);
+		if (cmd[i].full_cmd[0])
+			printf("cmd[%d].full_cmd[0] is %s$\n", i, cmd[i].full_cmd[0]);
+		if (cmd[i].full_cmd[1])
+			printf("cmd[%d].full_cmd[1] is %s$\t%p\n", i, cmd[i].full_cmd[1], &(*cmd[i].full_cmd[1]));
+		if (cmd[i].flags)
+			printf("cmd[%d].flags is %s$\n", i, cmd[i].flags);
+		if (cmd[i].input)
+			printf("cmd[%d].input is %s$\t%p\n", i, cmd[i].input, &(*cmd[i].input));
+		if (cmd[i].infile_name)
+			printf("cmd[%d].infile is %s$\n", i, cmd[i].infile_name);
+		if (cmd[i].outfile_name)
+			printf("cmd[%d].outfile is %s$\n", i, cmd[i].outfile_name);
+	}
+}
 
 //first time when cd is called, oldPWD is created, from second onwards, update.
 //error code 1
@@ -37,7 +61,7 @@ static void	go_dir(t_env **env, t_command *command)
 	char	*path;
 
 	temp = *((*env)->vars);
-	command->input = NULL;
+	// printf("temp = %p\n", &(*temp));
 	if (command->input == NULL)
 	{
 		temp = get_value((*env)->vars, "HOME");
@@ -45,7 +69,6 @@ static void	go_dir(t_env **env, t_command *command)
 			error_msg(1, "HOME not set\n", command);
 		else if (chdir(temp->value))
 			error_msg(1, "can't move to HOME directory\n", command);
-		free (temp);
 	}
 	else
 	{
@@ -66,19 +89,22 @@ void	ft_cd(t_command *command, t_env **env)
 
 	temp = *(*env)->vars;
 	old_pwd = getcwd(NULL, 0);
-	printf("old_pwd = %s\t%p\n", old_pwd, &(*old_pwd));
 	if (get_value((*env)->vars, "OLDPWD") == NULL)
 		set_value((*env)->vars, "OLDPWD", old_pwd);
 	else
 	{
 		temp = get_value(&temp, "OLDPWD");
+		free(temp->value);
 		temp->value = old_pwd;
 	}
+	// free(old_pwd);
+	print_command(command);
 	go_dir(env, command);
 	temp = NULL;
 	temp = get_value((*env)->vars, "PWD");
+	printf("temp value = %s\t%p\n", temp->value, &(*temp->value));
+	free(temp->value);
 	temp->value = getcwd(NULL, 0);
 	if (!temp->value)
 		printf("allocation fail!\n");
-	// free(temp);
 }
