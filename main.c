3460fda47fcfb47f0d5164d1517cc501a43d1be3
Rosa-Maria Makinen <rmakinen@c2r2p7.hive.fi>
2023-08-15 10:50:08 +0300

    everything seems to work, updated to the latest master

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rmakinen <rmakinen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2023/08/15 10:18:50 by rmakinen         ###   ########.fr       */
+/*   Updated: 2023/08/15 10:47:47 by rmakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,42 +47,42 @@
 // 	}
 // }
 
-static void	print_command(t_command *cmd)
-{
-	int	i;
-	int	j;
+// static void	print_command(t_command *cmd)
+// {
+// 	int	i;
+// 	int	j;
 
-	i = -1;
-	while (cmd[++i].command)
-	{
-		if (cmd[i].command)
-			printf("cmd[%d].command is %s\n", i, cmd[i].command);
-		if (cmd[i].full_cmd)
-		{
-			j = 0;
-			while (cmd[i].full_cmd[j])
-			{
-				printf("cmd[%d].full_cmd[%d] is %s\n", i, j, cmd[i].full_cmd[j]);
-				j++;
-			}
-		}
-		if (cmd[i].flags)
-			printf("cmd[%d].flags is %s\n", i, cmd[i].flags);
-		if (cmd[i].input)
-		{
-			j = 0;
-			while (cmd[i].input[j])
-			{
-				printf("cmd[%d].input[%d] is %s\n", i, j, cmd[i].input[j]);
-				j++;
-			}
-		}
-		if (cmd[i].infile_name)
-			printf("cmd[%d].infile is %s\n", i, cmd[i].infile_name);
-		if (cmd[i].outfile_name)
-			printf("cmd[%d].outfile is %s\n", i, cmd[i].outfile_name);
-	}
-}
+// 	i = -1;
+// 	while (cmd[++i].command)
+// 	{
+// 		if (cmd[i].command)
+// 			printf("cmd[%d].command is %s\n", i, cmd[i].command);
+// 		if (cmd[i].full_cmd)
+// 		{
+// 			j = 0;
+// 			while (cmd[i].full_cmd[j])
+// 			{
+// 				printf("cmd[%d].full_cmd[%d] is %s\n", i, j, cmd[i].full_cmd[j]);
+// 				j++;
+// 			}
+// 		}
+// 		if (cmd[i].flags)
+// 			printf("cmd[%d].flags is %s\n", i, cmd[i].flags);
+// 		if (cmd[i].input)
+// 		{
+// 			j = 0;
+// 			while (cmd[i].input[j])
+// 			{
+// 				printf("cmd[%d].input[%d] is %s\n", i, j, cmd[i].input[j]);
+// 				j++;
+// 			}
+// 		}
+// 		if (cmd[i].infile_name)
+// 			printf("cmd[%d].infile is %s\n", i, cmd[i].infile_name);
+// 		if (cmd[i].outfile_name)
+// 			printf("cmd[%d].outfile is %s\n", i, cmd[i].outfile_name);
+// 	}
+// }
 
 
 t_command	*ft_parser(t_data *ms, char **cmd_line)
@@ -120,7 +120,6 @@ static int	process_input_line(t_data *ms, char *input_line)
 		free_in_minishell(cmd, ms->struct_count);
 		return (1);
 	}
-	print_command(cmd);
 	execute_commands(cmd, ms->struct_count, &ms->env);
 	free_in_minishell(cmd, ms->struct_count);
 	return (0);
