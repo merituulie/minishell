0d4051b76f3710641d5653b5b25d9b9add22d1e4
chuchilini <vanesalees@gmail.com>
2023-08-18 14:29:21 +0300

    pwd change

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: meskelin <meskelin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yoonslee <yoonslee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 11:08:29 by rmakinen          #+#    #+#             */
-/*   Updated: 2023/08/08 13:46:36 by meskelin         ###   ########.fr       */
+/*   Updated: 2023/08/18 14:28:23 by yoonslee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,11 @@
 
 int	ft_pwd(t_env *env)
 {
-	t_node	*correct_node;
+	char	*pwd;
 
-	if (!env->vars)
-	{
-		ft_putstr_fd("Environment error.\n", 2, 1);
-		return (1);
-	}
-	correct_node = get_value(env->vars, "PWD");
-	ft_putstr_fd(correct_node->value, 1, 0);
+	(void)env;
+	pwd = getcwd(NULL, 0);
+	ft_putstr_fd(pwd, 1, 0);
 	ft_putchar_fd('\n', 1);
 	return (0);
 }
