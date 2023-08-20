4e6633dba5c8e104dd15381c88150fed1df97981
Josefina Husso <husso.josefina@gmail.com>
2023-08-20 08:57:17 +0300

    added Emmas changes from discord, checked with addresssanitizer the whole google docs exept last 4, docs updated

--- libft/libft.h
+++ libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: meskelin <meskelin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 17:07:21 by meskelin          #+#    #+#             */
-/*   Updated: 2023/08/15 11:24:11 by meskelin         ###   ########.fr       */
+/*   Updated: 2023/08/20 07:10:53 by jhusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,40 +18,37 @@
 # include <stdio.h>
 # include <stddef.h>
 
-long long	ft_atoll(const char *str);
+void		free_and_nullify(void *to_be_freed);
+void		free_char_array(char **str);
 int			ft_arrlen(char **array);
+long long	ft_atoll(const char *str);
 void		ft_bzero(void *s, size_t n);
 void		*ft_calloc(size_t nmemb, size_t size);
-char		*ft_free_null_str(char *str);
 void		ft_free_array(char **array);
+char		*ft_free_null_str(char *str);
 int			ft_isalnum(int c);
+int			ft_isalpha(int n);
+int			ft_isdigit(char c);
 char		*ft_itoa(int n);
 char		*ft_lltoa(long long n);
-int			ft_isalpha(int n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
 void		*ft_memcpy(void *dst, const void *src, size_t n);
 void		*ft_memset(void	*b, int c, size_t len);
-char		**ft_split(char const *s, char c);
-char		*ft_strcpy(char *dst, const char *src);
-char		*ft_strdup(const char *s1);
-size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t		ft_strlen(const char *s);
-int			ft_strncmp(const char *s1, const char *s2, size_t n);
-int			ft_strncmp_all(const char *s1, const char *s2);
-char		*ft_strtrim(char const *s1, char *set);
-char		*ft_substr(char const *s, unsigned int start, size_t len);
-int			ft_print_array(char **str);
 void		ft_putchar_fd(char c, int fd);
+void		ft_putnbr_fd(int n, int fd);
 void		ft_putstr_fd(char *s, int fd, int print_shellname);
-char		*ft_strjoin(char const *s1, char const *s2);
-char		*ft_strchr(const char *s, int c);
-char		*ft_strdup(const char *s1);
-size_t		ft_strlcat(char *dst, const char *src, size_t dstsize);
-void		free_char_array(char **str);
-int			ft_isdigit(char c);
+char		**ft_split(char const *s, char c);
 int			ft_strchr_index(const char *s, int c);
 char		*ft_strchr_null(const char *s, int c);
-void		ft_putnbr_fd(int n, int fd);
-void		free_and_nullify(void *to_be_freed);
-int			ft_memcmp(const void *s1, const void *s2, size_t n);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strdup(const char *s1);
+char		*ft_strjoin(char const *s1, char const *s2);
+size_t		ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlen(const char *s);
+int			ft_strncmp_all(const char *s1, const char *s2);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+char		*ft_strtrim(char const *s1, char *set);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
--- src/common/shlvl.c
+++ src/common/shlvl.c
@@ -3,14 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   shlvl.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmakinen <rmakinen@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 17:48:12 by meskelin          #+#    #+#             */
-/*   Updated: 2023/08/14 06:26:08 by rmakinen         ###   ########.fr       */
+/*   Updated: 2023/08/20 07:04:33 by jhusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
+
+static void	unset_oldpwd(t_node **temp)
+{
+	if (get_value(temp, "OLDPWD"))
+		delete_value(temp, "OLDPWD");
+}
 
 void	add_shlvl(t_env **env)
 {
@@ -19,6 +25,9 @@ void	add_shlvl(t_env **env)
 
 	temp = get_value((*env)->vars, "SHLVL");
 	shlvl = (int)ft_atoll(temp->value);
+	if (shlvl < 0)
+		shlvl = 0;
 	free(temp->value);
 	temp->value = ft_itoa(shlvl + 1);
+	unset_oldpwd(&temp);
 }
